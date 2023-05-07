c6221c0e8293b30533579040bccb668347a57ed8
Abdul Rehman <abdulrehmang571@gmail.com>
2023-05-07 08:40:28 +0500

    added the cards for displaying books

---
+++ script.js
@@ -0,0 +1,39 @@
+const myLibrary = [];
+
+function Book(title, author, pages, readIt) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readIt = readIt;
+}
+
+myLibrary.push(new Book('A study in Scarlet', 'Conan Doyle', '256', true));
+myLibrary.push(new Book('A sign of four', 'Conan Doyle', '280', false));
+myLibrary.push(new Book('Hounds of Baskerville', 'Conan Doyle', '266', true));
+
+function displayBoooks() {
+  const container = document.querySelector('.container');
+  myLibrary.forEach((book) => {
+    const section = document.createElement('section');
+
+    const title = document.createElement('h1');
+    title.textContent = book.title;
+
+    const author = document.createElement('p');
+    author.textContent = book.author;
+
+    const pages = document.createElement('p');
+    pages.textContent = book.pages;
+
+    const readButton = document.createElement('button');
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+
+    section.append(title, author, pages, readButton, removeButton);
+
+    container.appendChild(section);
+  });
+}
+
+displayBoooks();
