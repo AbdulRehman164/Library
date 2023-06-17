ac920acaa8f93ca90dcc649cdc795eb0ffbaab12
Abdul Rehman <abdulrehmang571@gmail.com>
2023-06-17 13:49:10 +0500

    made the displayBooks static method of the class Book

--- script.js
+++ script.js
@@ -18,52 +18,53 @@ class Book {
     this.pages = pages;
     this.readIt = readIt;
   }
-}
-function displayBooks() {
-  const container = document.querySelector('.container');
-  container.innerHTML = '';
-  myLibrary.forEach((book) => {
-    const section = document.createElement('section');
 
-    const title = document.createElement('h1');
-    title.textContent = book.title;
-    title.classList.add('cardTitle');
+  static displayBooks() {
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+    myLibrary.forEach((book) => {
+      const section = document.createElement('section');
 
-    const author = document.createElement('p');
-    author.textContent = book.author;
-    author.classList.add('cardauthor');
+      const title = document.createElement('h1');
+      title.textContent = book.title;
+      title.classList.add('cardTitle');
 
-    const pages = document.createElement('p');
-    pages.textContent = book.pages;
-    pages.classList.add('cardPages');
+      const author = document.createElement('p');
+      author.textContent = book.author;
+      author.classList.add('cardauthor');
 
-    const readButton = document.createElement('button');
-    readButton.classList.add('cardReadButton');
-    if (book.readIt === true) {
-      readButton.textContent = 'Not Read';
-      readButton.style.backgroundColor = 'lightcoral';
-    } else {
-      readButton.textContent = 'Read';
-      readButton.style.backgroundColor = 'lightgreen';
-    }
-    readButton.addEventListener('click', () => {
-      // eslint-disable-next-line no-param-reassign
-      book.readIt = !book.readIt;
-      displayBooks();
+      const pages = document.createElement('p');
+      pages.textContent = book.pages;
+      pages.classList.add('cardPages');
+
+      const readButton = document.createElement('button');
+      readButton.classList.add('cardReadButton');
+      if (book.readIt === true) {
+        readButton.textContent = 'Not Read';
+        readButton.style.backgroundColor = 'lightcoral';
+      } else {
+        readButton.textContent = 'Read';
+        readButton.style.backgroundColor = 'lightgreen';
+      }
+      readButton.addEventListener('click', () => {
+        // eslint-disable-next-line no-param-reassign
+        book.readIt = !book.readIt;
+        Book.displayBooks();
+      });
+
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
+
+      removeButton.addEventListener('click', () => {
+        section.remove();
+        myLibrary = myLibrary.filter((card) => card !== book);
+      });
+
+      section.append(title, author, pages, readButton, removeButton);
+
+      container.appendChild(section);
     });
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-
-    removeButton.addEventListener('click', () => {
-      section.remove();
-      myLibrary = myLibrary.filter((card) => card !== book);
-    });
-
-    section.append(title, author, pages, readButton, removeButton);
-
-    container.appendChild(section);
-  });
+  }
 }
 
 function addBookToLibrary() {
@@ -85,7 +86,7 @@ function addBookToLibrary() {
           readIt.checked
         )
       );
-      displayBooks();
+      Book.displayBooks();
       blurDiv.classList.remove('blur');
       form.style.display = 'none';
       form.reset();
